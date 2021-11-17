bc9ae022378fa71ac5d91e2ad7d43bd1abe8153b
cerdore <khn64@163.com>
2021-11-17 10:33:40 +0800

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const readme = `\
 ⏰ Updated on ${new Date().toUTCString()}
 ![Progress Bar CI](https://github.com/liununu/liununu/workflows/Progress%20Bar%20CI/badge.svg)\
 `
-<img align="right" src="https://github-readme-stats.vercel.app/api?username=Cerdore&show_icons=true">
+<img src="https://github-readme-stats.vercel.app/api?username=Cerdore&show_icons=true">
 
 🌱 I’m currently learning DB & Distributed system
 
