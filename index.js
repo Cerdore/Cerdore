33b77f8f3369ec511d588c470bd8764aeb3397b7
cerdore <khn64@163.com>
2021-11-17 10:46:50 +0800

    Update index.js

--- index.js
+++ index.js
@@ -15,13 +15,34 @@ function generateProgressBar() {
 
 const readme = `\
 ### Hi there 👋
+<!--
+**Cerdore/Cerdore** is a ✨ _special_ ✨ repository because its `README.md` (this file) appears on your GitHub profile.
 
 <img align="right" src="https://github-readme-stats.vercel.app/api?username=Cerdore&show_icons=true">
 
-🌱 I’m currently learning DB & Distributed system
+![Cerdore's github stats](https://github-readme-stats.vercel.app/api?username=Cerdore&show_icons=true&theme=radical)
+Here are some ideas to get you started:
+
+- 🔭 I’m currently working on ...
+- 🌱 I’m currently learning ...
+- 👯 I’m looking to collaborate on ...
+- 🤔 I’m looking for help with ...
+- 💬 Ask me about ...
+- 📫 How to reach me: ...
+- 😄 Pronouns: ...
+- ⚡ Fun fact: ...
+-->
+
+🌱 I’m currently learning DB & Distributed systems!
+
+💬 Wechat: c648038087
+
+[![Cerdore's github stats](https://github-readme-stats.vercel.app/api/top-langs/?username=Cerdore&hide=html)](https://github.com/anuraghazra/github-readme-stats)
+[![Top Langs](https://github-readme-stats.vercel.app/api?username=Cerdore&show_icons=true&count_private=true&line_height=40)](https://github.com/anuraghazra/github-readme-stats)
+<!-- <img align="right" src="https://github-readme-stats.vercel.app/api?username=Cerdore&show_icons=true"> -->
 
 
-⏳ Year progress ${progressBarOfThisYear} ${(progressOfThisYear * 100).toFixed(2)} %
+⏳ Year progress ${progressBarOfThisYear} ${(progressOfThisYear * 100).toFixed(1)} %
 ---
 ⏰ Updated on ${new Date().toUTCString()}
 ![Progress Bar CI](https://github.com/liununu/liununu/workflows/Progress%20Bar%20CI/badge.svg)\
