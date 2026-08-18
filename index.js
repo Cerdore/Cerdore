b43a92465fb62f1a70f76bb0faedcc8eae0d9d0a
Cerdore <khn64@163.com>
2026-08-18 20:30:05 +0800

    Remove GitHub stats images from index.js
    Removed GitHub stats images from index.js.

--- index.js
+++ index.js
@@ -33,9 +33,6 @@ Here are some ideas to get you started:
 - ⚡ Fun fact: ...
 -->
 
-[![Cerdore's github stats](https://github-readme-stats.vercel.app/api/top-langs/?username=Cerdore&hide=html,css)](https://github.com/anuraghazra/github-readme-stats)
-[![Top Langs](https://github-readme-stats.vercel.app/api?username=Cerdore&show_icons=true&count_private=true&line_height=40)](https://github.com/anuraghazra/github-readme-stats)
-<!-- <img align="right" src="https://github-readme-stats.vercel.app/api?username=Cerdore&show_icons=true"> -->
 
 
 ⏳ Year progress ${progressBarOfThisYear} ${(progressOfThisYear * 100).toFixed(1)} %
